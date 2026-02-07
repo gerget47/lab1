b4d22fce341eba8d9ca2a28ee0851d9ad93f2e56
Gerget Igor <gerget.igor@gmail.com>
2026-02-07 14:14:22 +0700

    поток 3

--- поток 3.js
+++ поток 3.js
@@ -1,34 +1,22 @@
-let age = 17;
+let num = 47;
 
-console.log("1 ");
-if (age >= 18 && age <= 30) {
-    console.log("Для молодежи");
-} else {
-    console.log("Для всех возрастов");
-}
- 
-console.log("\n2");
-if (age >= 18 && age <= 30) {
-    console.log("Для молодежи");
-} else if (age >= 1 && age <= 17) {
-    console.log("Для детей");
-} else {
-    console.log("Для всех возрастов");
-}
-
-// Можно проверить с разными значениями age
-console.log("\nПроверка с разными значениями:");
-let testAges = [15, 22, 35, 0, 18, 30];
-
-for (let i = 0; i < testAges.length; i++) {
-    let currentAge = testAges[i];
-    console.log("\nВозраст: " + currentAge);
-    
-    if (currentAge >= 18 && currentAge <= 30) {
-        console.log("Для молодежи");
-    } else if (currentAge >= 1 && currentAge <= 17) {
-        console.log("Для детей");
-    } else {
-        console.log("Для всех возрастов");
-    }
+switch(num) {
+    case 1:
+        console.log("На ветке сидит 1 ворона");
+        break;
+    case 2:
+    case 3:
+    case 4:
+        console.log(`На ветке сидит ${num} вороны`);
+        break;
+    case 5:
+    case 6:
+    case 7:
+    case 8:
+    case 9:
+    case 10:
+        console.log(`На ветке сидит ${num} ворон`);
+        break;
+    default:
+        console.log("Неверное число");
 }
