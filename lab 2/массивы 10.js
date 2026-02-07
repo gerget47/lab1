7adfa0769b2d98ce5b7b1bd108038d5f320afdd4
Gerget Igor <gerget.igor@gmail.com>
2026-02-07 18:17:44 +0700

    массив 10

--- lab 2/массивы 10.js
+++ lab 2/массивы 10.js
@@ -1,13 +1,6 @@
-function seq(n) {
-    if(n === 1) return 1;
-    let sum = 0;
-    for(let i = 1; i < n; i++) {
-        sum += seq(i);
-    }
-    return Math.sin(sum);
-}
-
-console.log(seq(1));
-console.log(seq(2));
-console.log(seq(3));
-console.log(seq(4));
+let arr = Array.from({length:10}, () => Math.floor(Math.random()*100));
+console.log("До:", arr);
+let maxIdx = arr.indexOf(Math.max(...arr));
+let minIdx = arr.indexOf(Math.min(...arr));
+[arr[maxIdx], arr[minIdx]] = [arr[minIdx], arr[maxIdx]];
+console.log("После:", arr);
