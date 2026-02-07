fa7adfb68bc325c391c93a9c72d3a89a51df1ac4
Gerget Igor <gerget.igor@gmail.com>
2026-02-07 15:55:21 +0700

    функции 4

--- lab 2/функции 4.js
+++ lab 2/функции 4.js
@@ -1,6 +1,7 @@
-let q = 1;
-let w = true;
-let e = "string";
-let r = 47n;
-let t = null;
-console.log(typeof(q), typeof(w), typeof(e), typeof(r), typeof(t));
+let variable = "Глобальная переменная"; 
+function f() { 
+  let variable = "Локальная переменная"; 
+  console.log(variable); 
+} 
+f(); 
+console.log(variable); 
