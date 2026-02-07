3fe1ac555a2e3872dd3d86877c142decc0c5869c
Gerget Igor <gerget.igor@gmail.com>
2026-02-07 14:18:15 +0700

    5 поток

--- поток 5.js
+++ поток 5.js
@@ -1,34 +1,7 @@
-let age = 17;
-
-console.log("1 ");
-if (age >= 18 && age <= 30) {
-    console.log("Для молодежи");
-} else {
-    console.log("Для всех возрастов");
-}
- 
-console.log("\n2");
-if (age >= 18 && age <= 30) {
-    console.log("Для молодежи");
-} else if (age >= 1 && age <= 17) {
-    console.log("Для детей");
-} else {
-    console.log("Для всех возрастов");
-}
-
-// Можно проверить с разными значениями age
-console.log("\nПроверка с разными значениями:");
-let testAges = [15, 22, 35, 0, 18, 30];
-
-for (let i = 0; i < testAges.length; i++) {
-    let currentAge = testAges[i];
-    console.log("\nВозраст: " + currentAge);
-    
-    if (currentAge >= 18 && currentAge <= 30) {
-        console.log("Для молодежи");
-    } else if (currentAge >= 1 && currentAge <= 17) {
-        console.log("Для детей");
-    } else {
-        console.log("Для всех возрастов");
+let sum = 0;
+for(let i = 1; i <= 15; i++) {
+    if(i !== 5 && i !== 7) {
+        sum += i;
     }
 }
+console.log(sum);
