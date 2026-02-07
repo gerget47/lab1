76c742f19fa5c191161e0d4a14089d829d5714b4
Gerget Igor <gerget.igor@gmail.com>
2026-02-07 15:57:13 +0700

    функции 5

--- lab 2/функции 5.js
+++ lab 2/функции 5.js
@@ -1,6 +1,10 @@
-let q = 1;
-let w = true;
-let e = "string";
-let r = 47n;
-let t = null;
-console.log(typeof(q), typeof(w), typeof(e), typeof(r), typeof(t));
+function calculateU(x, y, z) {
+    let max1 = Math.max(x, y);
+    let max2 = Math.max(x + y, z);
+    let max3 = Math.max(0.5, x + z);
+    let numerator = max1 + max2;
+    let denominator = Math.pow(max3, 2);
+    return numerator / denominator;
+}
+
+console.log(calculateU(1,2,3));
