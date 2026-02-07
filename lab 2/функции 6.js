28cec65f0b17de0cdb33136f8bdc07c1306b267a
Gerget Igor <gerget.igor@gmail.com>
2026-02-07 16:00:22 +0700

    функции 6

--- lab 2/функции 6.js
+++ lab 2/функции 6.js
@@ -1,6 +1,19 @@
-let q = 1;
-let w = true;
-let e = "string";
-let r = 47n;
-let t = null;
-console.log(typeof(q), typeof(w), typeof(e), typeof(r), typeof(t));
+function pP(...coords) {
+    let perim = 0;
+    let n = coords.length / 2;
+    
+    for(let i = 0; i < n; i++) {
+        let x1 = coords[2*i];
+        let y1 = coords[2*i + 1];
+        let x2 = coords[(2*i + 2) % coords.length];
+        let y2 = coords[(2*i + 3) % coords.length];
+        
+        let dx = x2 - x1;
+        let dy = y2 - y1;
+        perim += Math.sqrt(dx*dx + dy*dy);
+    }
+    
+    return perim;
+}
+
+console.log(pP(0,0, 3,0, 0,4));
